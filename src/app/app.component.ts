92bf10f74650a039b20c27f6b3daedd5b6e60ec2
Fabrizio Bugedo <fa.bugedo@duocuc.cl>
2025-05-25 18:03:55 -0400

    Server ionic funcionando

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
-  standalone: false,
+  standalone: true,
+  imports: [RouterOutlet, IonicModule]
 })
 export class AppComponent {
   constructor() {}
