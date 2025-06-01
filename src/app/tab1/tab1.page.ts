faa516a16562eadb2c1e641f4f9d09bc9f4aab21
Fabrizio Bugedo <fa.bugedo@duocuc.cl>
2025-05-31 21:04:36 -0400

    login y validaciones simples creadas

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -4,10 +4,22 @@ import { Component } from '@angular/core';
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
-  standalone: false,
+  standalone: false
 })
 export class Tab1Page {
 
+  usuario: string = '';
+  contrasena: string = '';
+
   constructor() {}
 
+  iniciarSesion() {
+    if (!this.usuario || !this.contrasena) {
+      alert('Por favor, complete ambos campos.');
+      return;
+    }
+
+    console.log('Usuario ingresado:', this.usuario);
+    console.log('Contraseña ingresada:', this.contrasena);
+  }
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,24 +1,26 @@
 import { Component } from '@angular/core';
 
-
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
-
-  standalone: false,
-
+  standalone: false
 })
 export class Tab2Page {
-  usuario: string = "";
-  nombre: string = "";
-  apellido: string = "";
+  usuario: string = '';
+  nombre: string = '';
+  apellido: string = '';
 
   constructor() {}
-  capturarDatos(){
-      console.log("Usuario --> " + this.usuario + "");
-      console.log("Nombre --> " + this.nombre + "");
-      console.log("Apellido --> " + this.apellido + "");
-  }
 
+  capturarDatos() {
+    if (!this.usuario || !this.nombre || !this.apellido) {
+      alert('Por favor, complete todos los campos.');
+      return;
+    }
+
+    console.log('Usuario -->', this.usuario);
+    console.log('Nombre -->', this.nombre);
+    console.log('Apellido -->', this.apellido);
+  }
 }
