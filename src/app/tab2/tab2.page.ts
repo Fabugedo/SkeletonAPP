f4088d481e0b44f9824fc32a2c4001797d75eaa7
Fabrizio Bugedo <fa.bugedo@duocuc.cl>
2025-05-31 20:34:16 -0400

    nuevo

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
+
   standalone: false,
+
 })
 export class Tab2Page {
+  usuario: string = "";
+  nombre: string = "";
+  apellido: string = "";
 
   constructor() {}
+  capturarDatos(){
+      console.log("Usuario --> " + this.usuario + "");
+      console.log("Nombre --> " + this.nombre + "");
+      console.log("Apellido --> " + this.apellido + "");
+  }
 
 }
